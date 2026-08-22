486e5e37c50b7ab65b3e3abd6fc60e3c6fc9e0ec
tinyblinker <2149934895@qq.com>
2026-08-22 14:30:34 +0800

    add Symphonia to convert audio files to pcm data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::path::Path;
+
+// use crate to implement this one
+fn audio_to_pcm(audio_path: &Path)->(){
+    unimplemented!()
+}
+
+fn FT(pcm_data: i32)->(){
+    unimplemented!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // transfer the audio file to pcm_data 
+    let audio_path = Path::new("_");
+    let pcm_data = audio_to_pcm(audio_path);
+
+    // do the "Fourier transform"
+    
 }
