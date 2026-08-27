9d64d01e071d7ea084ae9d7d1290e7827f6add20
tinyblinker <2149934895@qq.com>
2026-08-27 20:00:41 +0800

    try to break the "audio to pcm" progress to multiple small and clear functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use std::{env, fs::File, path::Path, thread::sleep, time::Duration};
 
 use symphonia::core::{
-    codecs::audio::AudioDecoderOptions,
-    formats::{probe::Hint, FormatOptions, TrackType},
+    codecs::audio::{AudioDecoder, AudioDecoderOptions},
+    formats::{probe::Hint, FormatOptions, FormatReader, Track, TrackType},
     io::MediaSourceStream,
     meta::MetadataOptions,
 };
 
-// // use crate to implement this one
-// fn audio_to_pcm(audio_path: &Path) -> () {
-//     unimplemented!()
-// }
-
-// fn FT(pcm_data: i32) -> () {
-//     unimplemented!()
-// }
-
-fn main() {
-    // Get command line arguments
-    let args: Vec<String> = env::args().collect();
-
+fn detect_audio_format(args: &Vec<String>) -> Box<dyn FormatReader> {
     // Create a media source.
     // "MediaSource" trait is automatically implemented for "File" among other type
     let file = Box::new((File::open(Path::new(&args[1]))).unwrap());
@@ -34,24 +22,39 @@ fn main() {
     // use the default options when reading and decoding
     let fmt_opts: FormatOptions = Default::default();
     let meta_opts: MetadataOptions = Default::default();
-    let dec_opts: AudioDecoderOptions = Default::default();
 
     // probe the media source stream for a format
-    let mut format = symphonia::default::get_probe()
+    let format = symphonia::default::get_probe()
         .probe(&hint, mss, fmt_opts, meta_opts)
         .unwrap();
 
+    // return format
+    format
+}
+
+fn get_the_track(format: Box<dyn FormatReader>) -> &Track {
     // get the default audio track
     let track = format.default_track(TrackType::Audio).unwrap();
+    track
+}
 
+fn create_the_decoder_for_track(track: &Track) -> Box<dyn AudioDecoder> {
+    let dec_opts: AudioDecoderOptions = Default::default();
     // create a decoder for the track
-    let mut decoder = symphonia::default::get_codecs()
+    let decoder = symphonia::default::get_codecs()
         .make_audio_decoder(
             track.codec_params.as_ref().unwrap().audio().unwrap(),
             &dec_opts,
         )
         .unwrap();
+    decoder
+}
 
+fn decode_the_track(
+    track: &Track,
+    decoder: Box<dyn AudioDecoder>,
+    format: &mut Box<dyn FormatReader>,
+) -> Vec<f32> {
     // store the track identifier, we'll use it to filter packages
     let track_id = track.id;
 
@@ -84,8 +87,8 @@ fn main() {
                 audio_buf.copy_to_slice_interleaved(&mut samples);
 
                 // sleep for a while and show the samples' datas
-                sleep(Duration::from_secs_f32(1.0));
-                println!("The samples(Vec<f32>) = {:?}", samples);
+                // sleep(Duration::from_secs_f32(1.0));
+                // println!("The samples(Vec<f32>) = {:?}", samples);
 
                 // Sum up the total number of samples
                 total_sample_count += samples.len();
@@ -99,10 +102,15 @@ fn main() {
             }
         }
     }
+    samples
+}
 
-    // // transfer the audio file to pcm_data
-    // let audio_path = Path::new("_");
-    // let pcm_data = audio_to_pcm(audio_path);
-
-    // do the "Fourier transform"
+fn main() {
+    // Get command line arguments
+    let args: Vec<String> = env::args().collect();
+    let format = detect_audio_format(&args);
+    let track: &Track = get_the_track(format);
+    let decoder = create_the_decoder_for_track(track);
+    let samples = decode_the_track(&track, decoder, &mut format);
+    println!("The samples(Vec<f32>) = {:?}", samples);
 }
