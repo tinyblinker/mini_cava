0db965f79945b503e77ce0743d4058638dc674ec
tinyblinker <2149934895@qq.com>
2026-09-06 19:11:05 +0800

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use anyhow::anyhow;
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
@@ -5,15 +7,16 @@ use cpal::{
     StreamConfig,
 };
 use ringbuf::{
-    traits::{Consumer, Producer, Split},
+    traits::{Consumer, Observer, Producer, Split},
     HeapRb,
 };
-use rustfft::{
-    num_complex::{Complex, Complex32},
-    FftPlanner,
-};
+use rustfft::{num_complex::Complex, FftPlanner};
 
 const FFT_SIZE: usize = 1024;
+
+fn clean_the_screen() -> () {
+    print!("\x1B[2J\x1B[H");
+}
 
 fn err_fn(err: Error) {
     match err.kind() {
@@ -22,6 +25,28 @@ fn err_fn(err: Error) {
         }
         _ => eprintln!("Stream error: {err}"),
     }
+}
+
+fn display_one_bin(bin: &f32) -> () {
+    println!("*****************");
+}
+
+fn display_fft_buffer(buffer: Vec<Complex<f32>>, config: StreamConfig) -> () {
+    // fft_data(transferred to Vec<f32>)
+    let fft_data: Vec<f32> = buffer
+        .iter()
+        .map(|fft_complex| fft_complex.norm())
+        .collect();
+    println!("fft_data.len() = {:?}", fft_data.len());
+
+    // 定义Vec<f32>(长度FFT_SIZE)中每一个数字是一个bin(视为cava显示中的一根柱子)
+    // 每两个bin之间相隔的频率等于sample_rate / FFT_SIZE
+    let freq_atom: f32 = config.sample_rate as f32 / FFT_SIZE as f32;
+    println!("freq_atom = {:?}", freq_atom);
+
+    // 清屏并显示(超级草率地)
+    clean_the_screen();
+    let _ = fft_data.iter().map(|bin| display_one_bin(bin));
 }
 
 fn run<T>(input_device: &Device, config: StreamConfig) -> Result<(), anyhow::Error>
@@ -36,7 +61,7 @@ where
     f32: FromSample<T>,
 {
     // Create RingBuffer for "input_data_fn" (producer), for the "RustFFT"(consumer)
-    let ring_capacity = FFT_SIZE * 8;
+    let ring_capacity = FFT_SIZE * 40;
     let ring = HeapRb::<T>::new(ring_capacity);
     let (mut producer, mut consumer) = ring.split();
     for _ in 0..(ring_capacity / 2) {
@@ -52,7 +77,7 @@ where
             data.len()
         );
         if pushed_slice_cnt < data.len() {
-            eprintln!("Producing fft input data too quickly!!");
+            eprintln!("Producing fft input data too fast!!");
         }
     };
 
@@ -67,24 +92,39 @@ where
     // play the stream(should be alive in this thread always!)
     input_stream.play()?;
 
-    // RustFFT
+    // RustFFT(PCM_data -> 频域数据<频谱>)
     let mut planner = FftPlanner::<f32>::new();
     let fft = planner.plan_fft_forward(FFT_SIZE);
-    loop {
-        let poped_data: &mut [T] = &mut [T::EQUILIBRIUM; FFT_SIZE];
-        let poped_data_len = consumer.pop_slice(poped_data);
+    let channels = config.channels;
+    let required_samples = FFT_SIZE * (channels as usize);
 
-        // 此处有问题,poped_data_len 始终 = 0
-        if poped_data_len < poped_data.len() {
-            eprintln!("Consuming fft input data too quickly!!");
+    loop {
+        // Data is consumed too fast!!! should be wait here
+        while consumer.occupied_len() < required_samples {
+            thread::sleep(Duration::from_millis(1));
         }
+
+        // vars about poped data
+        let mut poped_data = vec![T::EQUILIBRIUM; required_samples];
+        let poped_data_len = consumer.pop_slice(&mut poped_data);
         println!("poped_data_len = {:?}", poped_data_len);
+
+        // convert the data to satisfy the buffer
         let mut buffer: Vec<Complex<f32>> = poped_data
-            .iter()
-            .map(|&x| Complex32::new(x.to_sample::<f32>(), 0.0))
+            .chunks_exact_mut(2)
+            .map(|lr| {
+                Complex::<f32>::new(
+                    (lr[0].to_sample::<f32>() + lr[1].to_sample::<f32>()) / (channels as f32),
+                    0.0,
+                )
+            })
             .collect();
+        println!("buffer(before) = {:?}", buffer[10]);
         fft.process(&mut buffer);
-        println!("buffer.len() = {:?}", buffer.len());
+        println!("buffer(after) = {:?}", buffer[10]);
+
+        // display the fft buffer
+        display_fft_buffer(buffer, config);
     }
 }
 
